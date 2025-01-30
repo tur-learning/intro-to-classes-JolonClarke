56d4e9897dda57c4f3cbd22533b9cf829b2d8c34
JolonClarke <jolon.clarke@temple.edu>
2025-01-30 18:16:18 +0000

    The class I made is potato chips

---
+++ lab-sessions/classes/classes.py
@@ -0,0 +1,31 @@
+#class Person: # Classwork
+#    def __init__(self, name, age, height):
+#        self.name = name
+#        self.age = age
+#        self.height = height
+    
+#    def info(self):
+#        print(self.name, self.age, self.height)
+
+#    def change_age(self, new_age):
+#        self.age = new_age
+
+#----------------------------------------------------------------------------
+# Assignment:
+   
+class Chip:
+    def __init__(self, flavor, texture, size, brand):
+        self.flavor = flavor
+        self.texture = texture
+        self.size = size #cm
+        self.brand = brand
+
+    def info(self):
+        print(self.flavor, self.texture, self.size, self.brand)
+
+    def change_brand(self, new_brand):
+        self.brand = new_brand
+    
+    def chips_length(self): # how many centimeters if all chips in a bag are laid in a line
+        bag_length = self.size * 24
+        print(str(bag_length) + " cm")
---
+++ lab-sessions/classes/main.py
@@ -0,0 +1,8 @@
+from classes import Chip
+
+chip1 = Chip("BBQ", "ripple", 5, "Lays")
+
+chip1.info()
+chip1.change_brand("Pringles")
+chip1.info()
+chip1.chips_length()
